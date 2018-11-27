9b5e5cfb6c50c27b9a679780f923dce5ed14256a
Marco Antônio Dutra Teixeira <madtbr@gmail.com>
2018-11-27 11:01:15 -0200

    THEME mentor adicionado com sucesso

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-echo 'oiiiiiiiiiiiiiiiiiii';
 use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-
 class DefaultController extends AbstractController
 {
     /**
